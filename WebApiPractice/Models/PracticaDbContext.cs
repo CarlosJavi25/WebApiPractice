80a0821300bdc2eca02ea01287345f772def21b9
Admin <Admin@DESKTOP-MDCGKIV>
2023-09-10 15:10:07 -0400

    Add project files.

---
+++ WebApiPractice/Models/PracticaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPractice.Models;
+
+public partial class PracticaDbContext : DbContext
+{
+    public PracticaDbContext()
+    {
+    }
+
+    public PracticaDbContext(DbContextOptions<PracticaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    
+
+
+    
+}
---
+++ WebApiPractice/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiPractice.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Descripcion { get; set; } = null!;
+
+    public string? Marca { get; set; }
+
+    public DateTime Fechaingreso { get; set; }
+
+    public DateTime? Fechavencimiento { get; set; }
+
+    public DateTime Fechasalida { get; set; }
+
+    public decimal Precio { get; set; }
+
+    public int? Peso { get; set; }
+
+    public string? Unidades { get; set; }
+
+    public bool Itbis { get; set; }
+}
